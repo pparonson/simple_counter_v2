e6889beeaedf2086097d9ead674af1ae3a849baf
pparonson <preston.aronson@gmail.com>
2018-09-02 14:34:16 -0600

    revert to boilerplate

--- src/index.js
+++ src/index.js
@@ -12,47 +12,9 @@ const {pre, td, th, tr, tbody} = hh(h)
 
 const node = document.getElementById("app")
 
-// const el = document.createElement("div")
-// el.innerHTML = JSON.stringify(initModel, null, 2)
+const el = document.createElement("div")
+el.innerHTML = JSON.stringify(initModel, null, 2)
 
-// node.appendChild(el)
+node.appendChild(el)
 
 // app(initModel, update, view)
-
-// ____________________________________________________________________________
-
-const MEALS = [
-  {description: "breakfast", calories: 510}
-  , {description: "lunch", calories: 460}
-  , {description: "snack", calories: 235}
-]
-
-// Plan / fns to createElement:
-// cell, mealRow, headerRow, totalRow, mealBody, mealHeader, mealsTable
-function cell(_tag, _className, _data) {
-  return _tag({className: _className}, _data)
-}
-
-function tableRow(className, _mealRecord) {
-  return tr({className}, [
-    cell(td, "pa2", _mealRecord.description)
-    , cell(td, "pa2 tr", _mealRecord.calories)
-  ])
-}
-
-function tableBody(className, _meals) {
-  // rows is an array of table row elements
-  const rows = R.map(R.partial(tableRow, ["stripe-dark"]), _meals)
-  return tbody({className}, rows)
-}
-
-const _view = tableBody("", MEALS)
-
-// console.log(_view.outerHTML);
-// const el = document.createElement("td")
-// el.innerHTML = "Lunch"
-
-// Render the currentView node to a DOM node
-let rootNode = createElement(_view)
-
-node.appendChild(rootNode)
