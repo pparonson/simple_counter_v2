75777451dc86b4bf52ace7a5ca430a7224d10510
pparonson <preston.aronson@gmail.com>
2018-09-02 15:55:27 -0600

    tiny refactor index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,4 @@ import view from "./view"
 
 const node = document.getElementById("app")
 
-// const el = document.createElement("div")
-// el.innerHTML = JSON.stringify(initModel, null, 2)
-//
-// node.appendChild(el)
-
 app(initModel, view, update, node)
